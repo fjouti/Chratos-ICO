9c688342a45db39564b03eaa3111ea2367a67ba3
Kaptain Krayola <nhinrichs@gmail.com>
2017-02-02 21:42:14 -0600

    updates to calls to bitcoin-cli

--- wallet/server.js
+++ wallet/server.js
@@ -3,16 +3,36 @@ const app = express();
 const spawn = require("child_process").spawn;
 
 app.get("/", function(req, res, next){
+	console.log("healthcheck")
 	res.send("OK")
 })
 
 app.get('/balance/:wallet', function (req, res, next) {
-	res.send(req.params)
-	spawn("bitcoin-cli ", [`getbalanceget ${req.params.wallet}`])
+	var proc = spawn("bitcoin-cli", ["getbalance",req.params.wallet])
+	proc.stdout.on('data', function(data){
+	    res.send(data.toString().trim())
+	});
+
+	proc.stderr.on('data', function(data){
+		console.log(data.toString())
+	    res.status(500).send(data.toString().trim())
+	});
+
+	proc.on('closed', function(code){
+	    alert(`Child exited with code ${code}`);
+	});
 });
 
 app.post('/create', function (req, res, next) {
-	spawn("bitcoin-cli ", [`getnewaddress`])
+	var proc = spawn("bitcoin-cli", ["getnewaddress"])
+
+	proc.stdout.on('data', function(data){
+	    res.send(data.toString().trim())
+	});
+
+	proc.stderr.on('data', function(data){
+   		res.send(500, data.toString())
+	});
 });
 
 var server = app.listen(7575, function () {
